15be546ef86032efd980cd930a4675656829889a
Greg Connell <greg.connell@ff0000.com>
2017-11-22 15:27:53 -0800

    latest refactor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const _ = require('lodash');
 const mkdirp = require('mkdirp');
 
-
+const fbaCompiler = require('fba-compiler');
 const copier = require('./lib/copier.js');
 
 const debug = require('debug');
@@ -13,80 +13,88 @@ var log = debug('wp-plugin-assets');
 function AssetsPlugin(deploy, options) {
 	this.deploy = deploy;
 	this.options = options;
-};
+	/** TODO 
+			Document `this.options`:
+
+			this.options = {
+				assets: [
+					{
+						sources: [
+							list of asset paths
+						],
+						from: './source-context',
+						to: './destination-context',
+						// output from wp-plugin-payload 
+						payload: {
+							recompile: true/false,
+							modules: [
+								Webpack modules to compile
+							]
+						},
+						disable: true/false (switch to stop this asset-group from processing, either copying or compiling)
+					}
+				]
+			}
+	*/
+}
 
 
 AssetsPlugin.prototype.apply = function(compiler) {
 	compiler.plugin('emit', (compilation, callback) => {
 		log(this.options);
 
-		// emit non-compiled assets to deploy
-		this.emitNonCompiledAssets(
-			compilation,
-			this.deploy
-		)
-		
-		// TODO: add webpack-discovered binary assets for fba-payload
-		.then(() => {
-			if(this.deploy.payload.image.recompile || this.deploy.payload.font.recompile) {
-				log('HAS ASSETS TO COMPILE:');
-				this.deploy.payload.image.recompile = false;
-				this.deploy.payload.font.recompile = false;
-				const compileModules = this.deploy.payload.image.modules.concat(this.deploy.payload.font.modules)
-				compileModules.forEach((compileModule) => {
-					log(' ->', compileModule.rawRequest);
-				});
-			}
-
-			// TODO: <img> and background-image declarations would have to be rewritten to payload blobs
-		})
-
-		// generate fba-payload
-		.then(() => {
-			log('TODO: generate fba-payload');
-		})
-
-		// return to webpack flow
-		.then(() => {
-			callback();
-		})
-
-		.catch((err) => {
-			log(err);
-		});
-	});
-};
-
-
-
-AssetsPlugin.prototype.emitNonCompiledAssets = function(compilation) {
-	return new Promise((resolve, reject) => {
-		log('Emitting non-compiled assets...');
-
 		// prepare destination folders
 		prepareDeploy(this.deploy);
 
-		// run copiers
 		var promises = [];
-		for (var key in this.options.assets) {
-			if (!this.options.assets[key].disable) {
-				log(` - ${key}`);
+		var fbaAssets = [];
+		// iterate assets
+		this.options.assets.forEach((asset) => {
+			// fba-compile if asset has payload representation, and it is not disabled
+			if (asset.payload && !asset.payload.disabled) {
+				if (asset.payload.type == 'fba') {
+					asset.payload.modules.forEach((mod) => {
+						fbaAssets.push({
+							chunkType: asset.payload.chunkType,
+							data: mod.rawRequest,
+							to: asset.to
+						});
+					});
+				}
+			}
+			// otherwise copy the asset to deploy
+			else {
 				promises.push(
 					copier.copy(
 						this.deploy, 
 						this.options.assets[key]
 					)
-				);
+				);				
 			}
-		}
+		});
+		// TODO: have this be part of the promise-chain
+		fbaCompiler.compile(
+			this.deploy.output.fba,
+			fbaAssets
+		);
+		// TODO: <img> and background-image declarations would have to be rewritten to payload blobs
+
+
 		Promise.all(promises).then(() => {
 			resolve();
 		})
+		// return to webpack flow
+		.then(() => {
+			callback();
+		})
 		.catch((err) => {
 			log(err);
-		});
+		});		
 	});
-}
+};
+
+
+
 function prepareDeploy(deploy) {
 	if (!fs.existsSync(deploy.env.context.deploy)) {
 		mkdirp.sync(deploy.env.context.deploy);
