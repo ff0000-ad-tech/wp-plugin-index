816f76556755d205060884ba89a5e0cfa75e510e
Greg Connell <greg.connell@gregc-mbp.ff0000.com>
2018-04-09 17:25:17 -0700

    repaths import for @ff0000-ad-tech/debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const hooksRegex = require('@ff0000-ad-tech/hooks-regex')
 
-const debug = require('debug')
+const debug = require('@ff0000-ad-tech/debug')
 var log = debug('wp-plugin-index')
 
 const defaultOptions = {
